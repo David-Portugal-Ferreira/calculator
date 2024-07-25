62fbfc569e99efbf5858a05d8278c466e6b55648
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-07-25 09:37:05 +0100

    Make operate function for four basic operations.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function operate(number_1, operator, number_2) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(number_1, number_2);
+            console.log(result);
+            break;
+        case '-':
+            result = substract(number_1, number_2);
+            console.log(result);
+            break;
+        case '*':
+            result = multiply(number_1, number_2);
+            console.log(result);
+            break;
+        case '/':
+            result = divide(number_1, number_2);
+            console.log(result);
+            break;
+        default:
+            console.log('UNKNOW OPERATION')
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -9,5 +33,5 @@ function multiply(a, b) {
 }
 function divide(a, b) {
     if (b === 0) return "Can't divide by 0";
-    return a / b;
+    return +(a / b).toFixed(5);
 }
