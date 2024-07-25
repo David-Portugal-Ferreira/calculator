d546b7ebc1c9b12b24bed8c3ee6ddbb5c9040061
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-07-25 10:10:03 +0100

    Show result on HTML

--- script.js
+++ script.js
@@ -2,24 +2,33 @@ let number1;
 let operator;
 let number2;
 
+const displayResult = document.querySelector('.calculator-result');
+
 function operate(number_1, operator, number_2) {
     let result;
     switch (operator) {
         case '+':
             result = add(number_1, number_2);
             console.log(result);
+            displayResult.textContent = result;
+            number1 = result;
+            operator = '';
+            number2 = null;
             break;
         case '-':
             result = substract(number_1, number_2);
             console.log(result);
+            displayResult.textContent = result;
             break;
         case '*':
             result = multiply(number_1, number_2);
             console.log(result);
+            displayResult.textContent = result;
             break;
         case '/':
             result = divide(number_1, number_2);
             console.log(result);
+            displayResult.textContent = result;
             break;
         default:
             console.log('UNKNOW OPERATION')
