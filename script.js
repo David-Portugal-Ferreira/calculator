e50777639ae85be4ea0c3956191c21908b36ad15
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-07-25 12:33:19 +0100

    Make calculation logic for the four basic operations

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
 let number1;
 let operator;
 let number2;
+let input;
 
 const displayResult = document.querySelector('.calculator-result');
+const calculatorNumbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operations');
+const equalSign = document.querySelector('.equal');
+
+
+let arrNumbers = [...calculatorNumbers].map((button) => {
+    button.addEventListener('click', () => {
+        if (input === undefined) {
+            input = button.innerHTML;
+            if (input !== undefined) {
+                displayResult.textContent = input;
+            }
+            return;
+        }
+        input += button.innerHTML;
+        if (input !== undefined) {
+            displayResult.textContent = input;
+        }
+    })
+});
+
+let arrOperations = [...operations].map((operation) => {
+    operation.addEventListener('click', () => {
+        if (number1 === undefined) {
+            number1 = +input;
+            input = undefined;
+            console.log(number1);
+        } else if (number2 === undefined && input !== undefined) {
+            number2 = +input;
+            input = undefined;
+            console.log(number2);
+        }
+
+        let canOperate = checkNumbersAndOperator();
+        if (canOperate) operate(number1, operator, number2);
+        operator = operation.innerHTML;
+    });
+});
+
+equalSign.addEventListener('click', () => {
+    if (number2 === undefined && input !== undefined) {
+        number2 = +input;
+        input = undefined;
+        console.log(number2);
+    }
+    let canOperate = checkNumbersAndOperator();
+    if (canOperate) operate(number1, operator, number2);
+})
 
 function operate(number_1, operator, number_2) {
     let result;
@@ -11,24 +60,33 @@ function operate(number_1, operator, number_2) {
             result = add(number_1, number_2);
             console.log(result);
             displayResult.textContent = result;
-            number1 = result;
-            operator = '';
-            number2 = null;
+            number1 = +result;
+            operator = undefined;
+            number2 = undefined;
             break;
         case '-':
             result = substract(number_1, number_2);
             console.log(result);
             displayResult.textContent = result;
+            number1 = +result;
+            operator = undefined;
+            number2 = undefined;
             break;
         case '*':
             result = multiply(number_1, number_2);
             console.log(result);
             displayResult.textContent = result;
+            number1 = +result;
+            operator = undefined;
+            number2 = undefined;
             break;
         case '/':
             result = divide(number_1, number_2);
             console.log(result);
             displayResult.textContent = result;
+            number1 = +result;
+            operator = undefined;
+            number2 = undefined;
             break;
         default:
             console.log('UNKNOW OPERATION')
@@ -36,15 +94,41 @@ function operate(number_1, operator, number_2) {
 }
 
 function add(a, b) {
+    let isFloat = checkIsFloat(a, b);
+    if(isFloat) return (a + b).toFixed(5);
     return a + b;
 }
 function substract(a, b) {
+    let isFloat = checkIsFloat(a, b);
+    if(isFloat) return (a - b).toFixed(5);
     return a - b;
 }
 function multiply(a, b) {
+    let isFloat = checkIsFloat(a, b);
+    if(isFloat) return (a * b).toFixed(5);
     return a * b;
 }
 function divide(a, b) {
     if (b === 0) return "Can't divide by 0";
-    return +(a / b).toFixed(5);
+    let isFloat = checkIsFloat(a, b);
+    if(isFloat) return (a / b).toFixed(5);
+    return a / b;
+}
+
+function checkIsFloat(a, b) {
+    if (a % 1 !== 0 && b % 1 !== 0 ) {
+        return true;
+    }
+}
+
+function checkNumbersAndOperator() {
+    if (
+        number1 !== undefined &&
+        operator !== undefined &&
+        number2 !== undefined
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
